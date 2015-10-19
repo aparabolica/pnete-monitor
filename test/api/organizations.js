0d5792930f1c630053e005c80748c88c97484b21
Vitor George <vitor.george@gmail.com>
2015-10-19 19:25:34 -0200

    admin can change relations between users x organization

--- test/api/organizations.js
+++ test/api/organizations.js
@@ -181,9 +181,97 @@ describe('Users endpoints', function() {
       });
     });
 
-    // context('deny orgazination member');
-    //
-  })
+    context('deny organization member', function(){
+      it('should return 401', function(doneIt){
+        endpointUrl = restApiRoot
+                        + '/organizations/' + organization1.id
+                        +'/members/rel/'+user2.id;
+
+        request(app)
+          .put(endpointUrl)
+          .set('Authorization', user1AccessToken)
+          .send(payload)
+          .expect(401)
+          .expect('Content-Type', /json/)
+          .end(doneIt);
+      });
+    });
+
+  });
+
+  describe('PUT /users/{userId}/organizations/rel/{organizationId}', function(){
+
+    var endpointUrl;
+    var payload;
+
+    before(function(doneBefore){
+      endpointUrl = restApiRoot
+                      +'/users/'+user2.id
+                      + '/organizations/rel/' + organization1.id;
+      payload = {
+      }
+      doneBefore();
+    });
+
+    context('deny anonymous', function(){
+      it('should return 401', function(doneIt){
+        request(app)
+          .put(endpointUrl)
+          .send(payload)
+          .expect(401)
+          .expect('Content-Type', /json/)
+          .end(doneIt);
+      });
+    });
+
+    context('deny another user', function(){
+      it('should return 401', function(doneIt){
+        request(app)
+          .put(endpointUrl)
+          .set('Authorization', user1AccessToken)
+          .send(payload)
+          .expect(401)
+          .expect('Content-Type', /json/)
+          .end(doneIt);
+      });
+    });
+
+    context('deny own user', function(){
+      it('should return 401', function(doneIt){
+        request(app)
+          .put(endpointUrl)
+          .set('Authorization', user2AccessToken)
+          .send(payload)
+          .expect(401)
+          .expect('Content-Type', /json/)
+          .end(doneIt);
+      });
+    });
+
+    context('allow admin', function(){
+      it('should return 200 and org json', function(doneIt){
+
+        request(app)
+          .put(endpointUrl)
+          .set('Authorization', admin1AccessToken)
+          .send(payload)
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .end(function(err, res){
+            if (err) doneIt(err);
+            var body = res.body;
+
+            body.should.have.property('organizationId', organization1.id);
+            body.should.have.property('userId', user2.id);
+
+            doneIt();
+          });
+      });
+    });
+  });
+
+
+
 
   describe('GET /organizations', function(){
     it('should be allowed to everyone, but hiding private data');
