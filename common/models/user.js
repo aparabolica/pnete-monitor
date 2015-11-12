29272de7866ad7933a45479422f4c1dd7d2e9aea
Vitor George <vitor.george@gmail.com>
2015-11-12 13:00:14 -0200

    re-enable user routes

--- common/models/user.js
+++ common/models/user.js
@@ -15,19 +15,17 @@ module.exports = function(User) {
   User.disableRemoteMethod("updateAll", true);
   // User.disableRemoteMethod("updateAttributes", false);
 
-  User.disableRemoteMethod("find", true);
-  User.disableRemoteMethod("findById", true);
-  User.disableRemoteMethod("findOne", true);
-
-  User.disableRemoteMethod("deleteById", true);
+  // User.disableRemoteMethod("find", true);
+  // User.disableRemoteMethod("findById", true);
+  // User.disableRemoteMethod("findOne", true);
 
   User.disableRemoteMethod("createChangeStream", true);
   User.disableRemoteMethod("deleteById", true);
 
-  User.disableRemoteMethod("confirm", true);
-  User.disableRemoteMethod("count", true);
-  User.disableRemoteMethod("exists", true);
-  User.disableRemoteMethod("resetPassword", true);
+  // User.disableRemoteMethod("confirm", true);
+  // User.disableRemoteMethod("count", true);
+  // User.disableRemoteMethod("exists", true);
+  // User.disableRemoteMethod("resetPassword", true);
 
   // User.disableRemoteMethod('__count__accessTokens', false);
   // User.disableRemoteMethod('__create__accessTokens', false);
