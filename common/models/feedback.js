49dcd1156a912155727f9b392d971981cc57e572
Vitor George <vitor.george@gmail.com>
2015-11-16 03:27:48 -0200

    set organizationId in POST /feedback

--- common/models/feedback.js
+++ common/models/feedback.js
@@ -1,10 +1,21 @@
 module.exports = function(Feedback) {
 
+
   /*
    * Disable unwanted endpoints
    */
   Feedback.disableRemoteMethod("upsert", true);
   Feedback.disableRemoteMethod("updateAll", true);
   Feedback.disableRemoteMethod("createChangeStream", true);
+
+  Feedback.beforeRemote('create', function(ctx, instance, next){
+    var User = Feedback.app.models.user;
+    var requestUserId = ctx.req.accessToken.userId;
+    User.findById(requestUserId, function(err, user){
+      if (err) return next(err);
+      ctx.req.body.organizationId = user.organizationId;
+      next();
+    });
+  });
 
 };
