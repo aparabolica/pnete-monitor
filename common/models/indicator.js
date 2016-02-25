47f829af6b526e7c2b085bc7931fc7967149983d
Vitor George <vitor.george@gmail.com>
2016-02-25 18:57:15 -0300

    indicators export as csv

--- common/models/indicator.js
+++ common/models/indicator.js
@@ -1,3 +1,8 @@
+var json2csv = require('json2csv');
+var modelConfig = require('./indicator.json')
+var properties = Object.keys(modelConfig.properties);
+
+
 module.exports = function(Indicator) {
 
   /*
@@ -35,5 +40,29 @@ module.exports = function(Indicator) {
 
     next();
   });
+
+  Indicator.export = function(filter, res, doneExport) {
+    properties.push('axisId');
+    Indicator.find(filter, function(err, results){
+      json2csv({ data: results, fields: properties }, function(err, csv){
+        res.attachment('indicators.csv');
+        res.send(csv);
+      });
+    });
+  }
+
+  Indicator.remoteMethod(
+    'export',
+    {
+      http: { verb: 'get' },
+      accepts: [
+        {arg: 'filter', type: 'object'},
+        {arg: 'res', type: 'object', 'http': {source: 'res'}}
+      ],
+      returns: {arg: 'data', root: true}
+    }
+  )
+
+
 
 };
