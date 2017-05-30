293e375f28114d28aaee125b794a380215acb5ee
Miguel Peixe <miguel.peixe@aparabolica.com.br>
2017-05-30 09:41:40 -0300

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,11 @@
 set -e
 # allow the container to be started with `--user`
 if [ "$1" = 'node' ] || [ "$1" = 'nodemon' ]; then
+  echo "Updating permissions..."
   chown -R $APP_USER:$APP_USER $HOME
+  echo "Compiling assets..."
   exec gosu $APP_USER:$APP_USER $HOME/app/node_modules/grunt-cli/bin/grunt build
+  echo "Executing app process..."
 	exec gosu $APP_USER:$APP_USER "$@"
 fi
 exec "$@"
